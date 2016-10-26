69046860f598408e8f78303299d9daa9c02dd480
soeiner <sven.langner@tu-braunschweig.de>
2016-10-26 13:18:26 +0200

    works now using xkbcomp

--- mainloop.py
+++ mainloop.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python2.7
 
-import time
 import os
-import sys
 import re
 import socket
 import json
 
-from options import get_option, read_file
+from options import read_file
 
 
 class KeyMapping:
@@ -18,71 +16,81 @@ class KeyMapping:
         self.mapped_key = mapping_data["key"]
 
 
-def get_key_definition(map_file, key):
-    key = key.upper()
-    return re.search(r"key <" + key + r"> \{[^}]*\};", map_file).group(0)
+def get_xkb_code(keymap_data, key_code):
+    key_codes = re.search(r"xkb_keycodes *\".*\" *\{[^}]*\}", keymap_data).group(0)
+    key_code_search = re.search(r"<.{2,6}> *= *" + str(key_code) + r" *;", key_codes).group(0)
+    xkb_code = key_code_search.split("=")[0].strip(' ')[1:-1]
+    return xkb_code
 
 
-def update_key_definition(map_file, key, value):
-    cur_def = get_key_definition(map_file, key)
-    symbol_list_search = re.search(r"(?<=symbols).*\[(( ){0,30}.{0,20}( ){0,20},?){0,8}(?=\])", cur_def).group(0)
-    group, symbol_list_search = symbol_list_search.split("=")
-    group = group.replace("[", "").replace("]", "")
+def load_keymap(filename):
+    os.system("xkbcomp -xkb $DISPLAY " + filename)
+    return read_file(filename)
+
+
+def apply_keymap(filename, keymap_data):
+    with open(filename, "w") as f:
+        f.write(keymap_data)
+    os.system("xkbcomp -xkb %s $DISPLAY" % filename)
+
+
+def get_key_definition(keymap_data, xkb_code):
+    xkb_code = xkb_code.upper()
+    return re.search(r"key <" + xkb_code + r"> \{[^}]*\};", keymap_data).group(0)
+
+
+def update_key_definition(keymap_data, xkb_code, key):
+    old_key_def = new_key_def = get_key_definition(keymap_data, xkb_code)
+    symbol_list_search = re.search(r"(?<=symbols).*\[(( ){0,30}.{0,20}( ){0,20},?){0,8}\]", new_key_def).group(0)
+    group, old_symbol_list = symbol_list_search.split("=")
+    group = group.strip(' ')[1:-1]
     group = group[0].lower() + group[1:]
-    symbol_list = symbol_list_search.strip(' ')
-    num_entries = symbol_list.count(",") + 1
-    for i in range(num_entries, 8):
+    old_symbol_list = old_symbol_list.replace("[", "").replace("]", "").strip(' ')
+    symbol_list = old_symbol_list
+    print symbol_list
+    entries = symbol_list.split(',')
+    symbol_list = entries[0]
+    for i in range(1, 8):
         if i == 4 or i == 5:
-            symbol_list += ", " + str(value)
+            symbol_list += ", " + str(key)
+        elif i < len(entries):
+            symbol_list += ", " + str(entries[i])
         else:
             symbol_list += ", NoSymbol"
-    symbol_list += "]"
     print symbol_list
-    type_search = re.search(r"type(\[" + group + r"\])( )*=( )*\"(_|[A-Z]{0,20}){0,10}\"", cur_def)
+    type_search = re.search(r"type(\[" + group + r"\])( )*=( )*\"(_|[A-Z]{0,20}){0,10}\"", new_key_def)
     if not type_search:
-        type_search = re.search(r"type( )*=( )*\"(_|[A-Z]{0,20}){0,10}\"", cur_def)
+        type_search = re.search(r"type( )*=( )*\"(_|[A-Z]{0,20}){0,10}\"", new_key_def)
     type_key = type_search.group(0).split("=")[0]
-    cur_def = cur_def.replace(symbol_list_search, symbol_list)
-    cur_def = cur_def.replace(type_search.group(0), type_key + "=\"EIGHT_LEVEL_SEMIALPHABETIC\"")
-    print cur_def
+    new_key_def = new_key_def.replace(old_symbol_list, symbol_list)
+    new_key_def = new_key_def.replace(type_search.group(0), type_key + "=\"EIGHT_LEVEL_SEMIALPHABETIC\"")
+    keymap_data = keymap_data.replace(old_key_def, new_key_def)
+    return keymap_data
 
 
 def update_keymap():
     keymap = {}
-    os.system("xkbcomp -xkb $DISPLAY keymap")
-    with open("test") as f:
-        keymap_file = f.read()
-    with open("config.json") as f:
-        config = json.loads(f.read())
-    keymap_data = config["keymap"]
-    modifiers = []
-    cur_caps = get_key_definition(keymap_file, "CAPS")
-    cur_mode_switch = get_key_definition(keymap_file, "MDSW")
-    cur_mod_map = re.search(r"modifier_map .{3,8} \{.*\n?.*<MDSW>.*\n?.*\}", keymap_file).group(0)
-    keymap_file = keymap_file.replace(cur_caps, read_file("assets/caps_default"))
-    keymap_file = keymap_file.replace(cur_mode_switch, read_file("assets/mdsw_default"))
-    keymap_file = keymap_file.replace(cur_mod_map, "modifier_map Mod3 { <MDSW> }")
-    update_key_definition(keymap_file, "AC02", "Up")
-    for mapping in keymap_data:
-        try:
-            for mod in mapping["modifiers"]:
-                if mod not in modifiers:
-                    modifiers.append(mod)
-            code = mapping["code"]
-            original_key = get_option("Xmodmap_tmp", "keycode " + str(code)).split(" ")[0]
-            if code not in keymap:
-                keymap[code] = []
-            keymap[code].append(KeyMapping(mapping, original_key))
-            os.system("xmodmap \"keycode %s = %s %s %s %s\"" % (str(code), original_key, original_key.upper(),
-                                                                original_key, original_key.upper()))
-        except Exception as e:
-            print e
-    for mod in modifiers:
-        os.system("xmodmap \"keycode %s = NoSymbol\"" % (str(mod)))
+    keymap_data = load_keymap("keymap")
 
+    # set up modifier keys
+    cur_caps = get_key_definition(keymap_data, "CAPS")
+    cur_mode_switch = get_key_definition(keymap_data, "MDSW")
+    cur_mod_map = re.search(r"modifier_map .{3,8} \{[^}]*<MDSW>[^}]*\}", keymap_data).group(0)
+    keymap_data = keymap_data.replace(cur_caps, read_file("assets/caps_default"))
+    keymap_data = keymap_data.replace(cur_mode_switch, read_file("assets/mdsw_default"))
+    keymap_data = keymap_data.replace(cur_mod_map, "modifier_map Mod3 { <MDSW> }")
 
-    with open("keymap", "w") as f:
-        f.write(keymap_file)
+    config = json.loads(read_file("config.json"))
+    mapping_data = config["mapping"]
+
+    for mapping in mapping_data:
+        code = mapping["code"]
+        xkb_code = get_xkb_code(keymap_data, code)
+        print code, xkb_code
+        replacement_key = mapping["key"]
+        keymap_data = update_key_definition(keymap_data, xkb_code, replacement_key)
+
+    apply_keymap("keymap", keymap_data)
 
     return keymap
 
@@ -92,7 +100,7 @@ def main():
     #    time.sleep(5)
 
     os.chdir(os.path.dirname(__file__))
-    keymap = update_keymap()
+    update_keymap()
 
     s = socket.socket()
     s.bind(("localhost", 24679))
