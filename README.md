d2fdaa2d850b3ed72c1a35aa96ce4341d8533d66
soeiner <sven.langner@tu-braunschweig.de>
2016-10-27 01:11:05 +0200

    possibility to use 2 overlays at the same time

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
-# System wide usable navigation keys on the home row
-This python script turns the Caps Lock key into a 'modifier'.
+# Coding without jumping around on the keyboard and twisting your fingers? Yes.
+This little program allows you to easily set up overlays for your xkb keyboard.<br>
+Specify a key (by default Caps Lock), disable its normal behaviour and use it as an activator for
+another key layer on your keyboard.<br>
+This also allows usage of navigation keys such as Up, Down, Left and Right on the home row.
 
-## Behaviour while caps is hold down
-- level 3 shift is activated (the same as pressing 'Alt Gr')
-- i, j, k and l can be used as arrow keys
-- h becomes Pos1, oe End, u PageUp, o PageDown
+## Configuration File
+- easily define your own mappings in a JSON-based configuration file
+- usage explained in the file itself
 
 ## Installation
-- you will need xdotool for input manipulation<br>
+<!---
+you will need xdotool for input manipulation<br>
 (if you do not have superuser rights, you can also download it and compile it yourself)
 <pre>sudo apt-get install xdotool</pre>
+-->
 
 - clone this repository to a path_of_your_choice.
 <pre>git clone https://github.com/soeiner/homerow-arrowkeys.git path_of_your_choice</pre>
@@ -17,7 +21,7 @@ This python script turns the Caps Lock key into a 'modifier'.
 - change working directory to the installation directory
 <pre>cd path_of_your_choice</pre>
 
-- make it executable
+- make scripts executable
 <pre>chmod +x mainloop.py resume.py</pre>
 
 - test it
@@ -27,5 +31,5 @@ now open an editor and try pressing some keys with and without caps held down.
 - add it to start up (tested on Ubuntu based systems)<br>
 Go to the launcher and open the program 'Startup Applications'.<br>Click on 'Add'.<br>Enter some name.<br>Click on 'Browse'.<br>Navigate to 'mainloop.py'.
 
-- add it to after resume execution
+- add it to resume directory so the overlays still work after resume
 <pre>sudo cp resume.py /etc/pm/sleep.d/</pre>
--- mainloop.py
+++ mainloop.py
@@ -10,115 +10,156 @@ import sys
 from options import read_file
 
 
-def load_keymap(filename):
-    os.system("xkbcomp -xkb $DISPLAY " + filename)
-    return read_file(filename)
+class KeyMapper:
+    def __init__(self):
+        self.keymap_data = ""
+        self.keymap_file = "keymap"
+        self.main()
 
+    def get_interpret_section(self, stroke):
+        re_stroke = stroke.replace("+", "\+").replace("(", "\(").replace(")", "\)")
+        interpret_section = re.search(r"interpret *" + re_stroke + r" *\{[^}]*\}( |\n)*;", self.keymap_data).group(0)
+        return interpret_section
 
-def apply_keymap(filename, keymap_data):
-    with open(filename, "w") as f:
-        f.write(keymap_data)
-    os.system("xkbcomp -xkb %s $DISPLAY" % filename)
+    def get_xkb_code(self, key_code):
+        key_codes = self.get_xkb_keycodes_section()
+        key_code_search = re.search(r"<.{2,6}> *= *" + str(key_code) + r" *;", key_codes).group(0)
+        xkb_code = key_code_search.split("=")[0].strip(' ')[1:-1]
+        return xkb_code
 
+    def get_xkb_keycodes_section(self):
+        return re.search(r"xkb_keycodes *\".*\" *\{[^}]*\}", self.keymap_data).group(0)
 
-def get_interpret(keymap_data, stroke):
-    re_stroke = stroke.replace("+", "\+").replace("(", "\(").replace(")", "\)")
-    interpret_section = re.search(r"interpret *" + re_stroke + r" *\{[^}]*\}( |\n)*;", keymap_data).group(0)
-    print interpret_section
-    return interpret_section
+    def get_xkb_symbols_section(self):
+        return re.search(r"xkb_symbols *\".*\" *\{([^{]+(name|key)[^{]+\{[^}]+\};)*", self.keymap_data).group(0)
 
+    def get_keysym_section(self, xkb_code):
+        xkb_code = xkb_code.upper()
+        return re.search(r"key <" + xkb_code + r"> \{[^}]*\};", self.keymap_data).group(0)
 
-def get_key_definition(keymap_data, xkb_code):
-    xkb_code = xkb_code.upper()
-    return re.search(r"key <" + xkb_code + r"> \{[^}]*\};", keymap_data).group(0)
-
-
-def get_xkb_code(keymap_data, key_code):
-    key_codes = re.search(r"xkb_keycodes *\".*\" *\{[^}]*\}", keymap_data).group(0)
-    key_code_search = re.search(r"<.{2,6}> *= *" + str(key_code) + r" *;", key_codes).group(0)
-    xkb_code = key_code_search.split("=")[0].strip(' ')[1:-1]
-    return xkb_code
-
-
-def get_xkb_symbols_section(keymap_data):
-    return re.search(r"xkb_symbols *\".*\" *\{([^{]+(name|key)[^{]+\{[^}]+\};)*", keymap_data).group(0)
-
-
-def add_overlay_key(keymap_data, xkb_code, overlay_xkb_code):
-    old_key_def = new_key_def = get_key_definition(keymap_data, xkb_code)
-    new_key_def = new_key_def.replace("}", ",\n    overlay1 = <" + overlay_xkb_code + "> \n}")
-    keymap_data = keymap_data.replace(old_key_def, new_key_def)
-    return keymap_data
-
-
-def update_keymap():
-    keymap_data = load_keymap("keymap")
-
-    # set up modifier keys
-    cur_caps = get_key_definition(keymap_data, "CAPS")
-    keymap_data = keymap_data.replace(cur_caps, read_file("assets/caps_default"))
-
-    config = json.loads(read_file("config.json"))
-    mapping_data = config["mapping"]
-
-    available_key_codes = []
-    xkb_symbols_section = get_xkb_symbols_section(keymap_data)
-    for i in range(9, 255):
-        try:
-            xkb_code = get_xkb_code(keymap_data, i)
+    def get_unused_xkb_codes(self):
+        all_codes = self.get_xkb_codes()
+        codes = []
+        xkb_symbols_section = self.get_xkb_symbols_section()
+        for xkb_code in all_codes:
             if xkb_code not in xkb_symbols_section:
-                print "No Symbols defined: " + xkb_code
-                available_key_codes.append(xkb_code)
-        except AttributeError as e:
-            print e
-            print "Key Code " + str(i) + " is not defined."
+                codes.append(xkb_code)
+        return codes
 
-    for mapping in mapping_data:
-        if "mapped_xkb_code" not in mapping:
-            key = mapping["key"]
-            if len(available_key_codes) == 0:
-                print "NO FREE KEY CODES LEFT FOR MAPPING!!!"
-                break
-            else:
-                xkb_code = available_key_codes.pop()
-                print xkb_code
-                mapping["mapped_xkb_code"] = xkb_code
-                new_key_data = "\n key <" + xkb_code + "> {[" + key + "]};"
-                xkb_symbols_section = old_xkb_symbols_section = get_xkb_symbols_section(keymap_data)
-                xkb_symbols_section += new_key_data
-                keymap_data = keymap_data.replace(old_xkb_symbols_section, xkb_symbols_section)
+    def get_xkb_codes(self):
+        codes = []
+        for i in range(9, 255):
+            try:
+                xkb_code = self.get_xkb_code(i)
+                codes.append(xkb_code)
+            except AttributeError as e:
+                print "Key code #" + str(i) + " has no xkb_code defined in xkb_keymap."
+        return codes
 
-    for mapping in mapping_data:
-        if "mapped_xkb_code" in mapping:
-            code = mapping["code"]
-            xkb_code = get_xkb_code(keymap_data, code)
-            overlay_xkb_code = mapping["mapped_xkb_code"]
-            keymap_data = add_overlay_key(keymap_data, xkb_code, overlay_xkb_code)
+    def add_overlay_key(self, xkb_code, overlay_xkb_code, num_overlay):
+        old_key_def = new_key_def = self.get_keysym_section(xkb_code)
+        if "overlay" in old_key_def:
+            old_overlay = re.search(r",\n *overlay[0-9] *= *<.{2,6}>", old_key_def).group(0)
+            new_key_def = new_key_def.replace(old_overlay, "")
+            print xkb_code + " CAN NOT HAVE ANOTHER OVERLAY. DELETING PREVIOUS OVERLAY!"
+        new_key_def = new_key_def.replace("}", ",\n overlay" + str(num_overlay) + " = <" + overlay_xkb_code + "> \n}")
+        self.keymap_data = self.keymap_data.replace(old_key_def, new_key_def)
 
-    keymap_data = keymap_data.replace(get_interpret(keymap_data, "Overlay1_Enable+AnyOfOrNone(all)"), read_file("assets/interpret_overlay1"))
-
-    apply_keymap("keymap", keymap_data)
-
-    return keymap_data
+    def map_overlays(self, mapping_data, num_overlay):
+        # add overlays to keys
+        for mapping in mapping_data:
+            if "mapped_xkb_code" in mapping:
+                code = mapping["key_code"]
+                overlay_xkb_code = mapping["mapped_xkb_code"]
+                xkb_code = self.get_xkb_code(code)
+                self.add_overlay_key(xkb_code, overlay_xkb_code, num_overlay)
 
 
-def main():
-    if "nosleep" not in sys.argv:
-        time.sleep(5)
+    def disable_overlay_key_toggling(self, num_overlay):
+        num_overlay = str(num_overlay)
+        self.keymap_data = self.keymap_data.replace(self.get_interpret_section("Overlay%s_Enable+AnyOfOrNone(all)" % num_overlay),
+                                          read_file("assets/interpret_overlay" + num_overlay))
 
-    os.chdir(os.path.dirname(__file__))
-    keymap_data = update_keymap()
+    def set_overlay_enable_key(self, xkb_code, num_overlay):
+        # set up modifier key
+        cur_caps = self.get_keysym_section(xkb_code)
+        keysym = read_file("assets/overlay_enable_keysym").replace("####", xkb_code).replace("$$$$", str(num_overlay))
+        self.keymap_data = self.keymap_data.replace(cur_caps, keysym)
 
-    s = socket.socket()
-    s.bind(("localhost", 24679))
-    s.listen(1)
+    def create_keysym_sections(self, mapping_data):
+        available_key_codes = self.get_unused_xkb_codes()
+        # Use available xkb_codes to map user defined key strokes
+        for mapping in mapping_data:
+            if "mapped_xkb_code" not in mapping:
+                overlay_key = mapping["overlay_key"]
+                if len(available_key_codes) == 0:
+                    print "============================================================"
+                    print "NO FREE KEY CODES AVAILABLE FOR MAPPING!!! "
+                    print "please log back again"
+                    print "you may also have exceeded the number of available key codes"
+                    print "============================================================"
+                    break
+                else:
+                    xkb_code = available_key_codes.pop()
+                    print "Found free (unused) xkb_code and mapping it now: " + xkb_code
+                    mapping["mapped_xkb_code"] = xkb_code
+                    new_key_data = "\n key <" + xkb_code + "> {[" + overlay_key + "]};"
+                    xkb_symbols_section = old_xkb_symbols_section = self.get_xkb_symbols_section()
+                    xkb_symbols_section += new_key_data
+                    self.keymap_data = self.keymap_data.replace(old_xkb_symbols_section, xkb_symbols_section)
 
-    while True:
+    def update_keymap(self):
+        self.capture_keymap(self.keymap_file)
+        config = json.loads(read_file("config.json"))
+
+        def update_overlay(num_overlay):
+            overlay = "overlay" + str(num_overlay)
+            if config[overlay]["xkb_code"] != "disabled":
+                self.create_keysym_sections(config[overlay]["mapping"])
+                self.set_overlay_enable_key(config[overlay]["xkb_code"], num_overlay)
+                self.map_overlays(config[overlay]["mapping"], num_overlay)
+                if config[overlay]["mode"] == "hold":
+                    self.disable_overlay_key_toggling(num_overlay)
+
+        update_overlay(1)
+        update_overlay(2)
+
+        with open(self.keymap_file, "w") as f:
+            f.write(self.keymap_data)
+        self.load_keymap_file(self.keymap_file)
+
+    def capture_keymap(self, filename):
+        os.system("xkbcomp -xkb $DISPLAY " + filename)
+        self.keymap_data = read_file(filename)
+
+    def load_keymap_file(self, filename):
+        print "Applying keymap settings..."
+        if "rror" not in os.popen("xkbcomp -xkb %s $DISPLAY" % filename).read():
+            print "Successfully applied settings."
+            os.remove(filename)
+
+    def main(self):
+        #if "nosleep" not in sys.argv:
+        #    time.sleep(5)
+
+        os.chdir(os.path.dirname(__file__))
+        self.update_keymap()
+
         try:
-            s.accept()
-            apply_keymap("keymap", keymap_data)
-        except KeyboardInterrupt:
-            break
+            s = socket.socket()
+            s.bind(("localhost", 24679))
+            s.listen(1)
+
+            while True:
+                try:
+                    s.accept()
+                    time.sleep(5)
+                    self.load_keymap_file(self.keymap_file)
+                except KeyboardInterrupt:
+                    break
+        except Exception as e:
+            print e
+            print "Another instance of this Software is already running. Kill it with 'pkill python2.7'."
 
 
-main()
+KeyMapper()
