b04875a8ab97ace2970f90fa56040c8710052119
soeiner <sven.langner@tu-braunschweig.de>
2016-10-28 01:21:41 +0200

    bug fix

--- mainloop.py
+++ mainloop.py
@@ -14,7 +14,31 @@ class KeyMapper:
     def __init__(self):
         self.keymap_data = ""
         self.keymap_file = "keymap"
+        self.generated_xkb_codes_file = "assets/generated_xkb_codes"
+        self.used_xkb_codes = self.get_generated_xkb_codes()
         self.main()
+
+    def store_used_xkb_codes(self):
+        file_data = ""
+        xkb_codes = []
+        # remove double elements
+        for xkb_code in self.used_xkb_codes:
+            if xkb_code not in xkb_codes:
+                xkb_codes.append(xkb_code)
+        self.used_xkb_codes = xkb_codes
+        for xkb_code in self.used_xkb_codes:
+            file_data += "\n" + xkb_code
+        with open(self.generated_xkb_codes_file, "w") as f:
+            f.write(file_data)
+
+    def get_generated_xkb_codes(self):
+        if os.path.isfile(self.generated_xkb_codes_file):
+            codes = read_file(self.generated_xkb_codes_file).split("\n")
+            while '' in codes:
+                codes.remove('')
+            return codes
+        else:
+            return []
 
     def get_interpret_section(self, stroke):
         re_stroke = stroke.replace("+", "\+").replace("(", "\(").replace(")", "\)")
@@ -65,7 +89,7 @@ class KeyMapper:
         new_key_def = new_key_def.replace("}", ",\n overlay" + str(num_overlay) + " = <" + overlay_xkb_code + "> \n}")
         self.keymap_data = self.keymap_data.replace(old_key_def, new_key_def)
 
-    def map_overlays(self, mapping_data, num_overlay):
+    def map_overlay_keys(self, mapping_data, num_overlay):
         # add overlays to keys
         for mapping in mapping_data:
             if "mapped_xkb_code" in mapping:
@@ -74,17 +98,17 @@ class KeyMapper:
                 xkb_code = self.get_xkb_code(code)
                 self.add_overlay_key(xkb_code, overlay_xkb_code, num_overlay)
 
-
     def disable_overlay_key_toggling(self, num_overlay):
         num_overlay = str(num_overlay)
-        self.keymap_data = self.keymap_data.replace(self.get_interpret_section("Overlay%s_Enable+AnyOfOrNone(all)" % num_overlay),
-                                          read_file("assets/interpret_overlay" + num_overlay))
+        self.keymap_data = self.keymap_data.replace(
+            self.get_interpret_section("Overlay%s_Enable+AnyOfOrNone(all)" % num_overlay),
+            read_file("assets/interpret_overlay" + num_overlay))
 
     def set_overlay_enable_key(self, xkb_code, num_overlay):
         # set up modifier key
-        cur_caps = self.get_keysym_section(xkb_code)
+        old_keysym_section = self.get_keysym_section(xkb_code)
         keysym = read_file("assets/overlay_enable_keysym").replace("####", xkb_code).replace("$$$$", str(num_overlay))
-        self.keymap_data = self.keymap_data.replace(cur_caps, keysym)
+        self.keymap_data = self.keymap_data.replace(old_keysym_section, keysym)
 
     def create_keysym_sections(self, mapping_data):
         available_key_codes = self.get_unused_xkb_codes()
@@ -95,8 +119,8 @@ class KeyMapper:
                 if len(available_key_codes) == 0:
                     print "============================================================"
                     print "NO FREE KEY CODES AVAILABLE FOR MAPPING!!! "
-                    print "please log back again"
-                    print "you may also have exceeded the number of available key codes"
+                    print "please log back in again"
+                    print "if this problem persists, you may also have exceeded the number of available key codes"
                     print "============================================================"
                     break
                 else:
@@ -107,17 +131,26 @@ class KeyMapper:
                     xkb_symbols_section = old_xkb_symbols_section = self.get_xkb_symbols_section()
                     xkb_symbols_section += new_key_data
                     self.keymap_data = self.keymap_data.replace(old_xkb_symbols_section, xkb_symbols_section)
+                    self.used_xkb_codes.append(xkb_code)
 
     def update_keymap(self):
         self.capture_keymap(self.keymap_file)
         config = json.loads(read_file("config.json"))
+
+        # remove xkb_codes added during last execution
+        while len(self.used_xkb_codes) > 0:
+            previously_used_xkb_code = self.used_xkb_codes.pop()
+            try:
+                self.keymap_data = self.keymap_data.replace(self.get_keysym_section(previously_used_xkb_code), "")
+            except AttributeError as e:
+                pass
 
         def update_overlay(num_overlay):
             overlay = "overlay" + str(num_overlay)
             if config[overlay]["xkb_code"] != "disabled":
                 self.create_keysym_sections(config[overlay]["mapping"])
                 self.set_overlay_enable_key(config[overlay]["xkb_code"], num_overlay)
-                self.map_overlays(config[overlay]["mapping"], num_overlay)
+                self.map_overlay_keys(config[overlay]["mapping"], num_overlay)
                 if config[overlay]["mode"] == "hold":
                     self.disable_overlay_key_toggling(num_overlay)
 
@@ -133,14 +166,19 @@ class KeyMapper:
         self.keymap_data = read_file(filename)
 
     def load_keymap_file(self, filename):
+        self.store_used_xkb_codes()
         print "Applying keymap settings..."
-        if "rror" not in os.popen("xkbcomp -xkb %s $DISPLAY" % filename).read():
+        output = os.popen("xkbcomp -xkb %s $DISPLAY" % filename).read()
+        if "rror" not in output:
             print "Successfully applied settings."
             os.remove(filename)
+        else:
+            print "Error while applying settings."
+        print "INFO: still " + str(len(self.get_unused_xkb_codes())) + " xkb_codes available for custom mappings."
 
     def main(self):
-        #if "nosleep" not in sys.argv:
-        #    time.sleep(5)
+        if "nosleep" not in sys.argv:
+            time.sleep(5)
 
         os.chdir(os.path.dirname(__file__))
         self.update_keymap()
@@ -158,8 +196,8 @@ class KeyMapper:
                 except KeyboardInterrupt:
                     break
         except Exception as e:
-            print e
-            print "Another instance of this Software is already running. Kill it with 'pkill python2.7'."
+            print e, ". Another instance of this Software is already running. Kill it with " \
+                     "'pkill python2.7' if you want suspend/resume to work correctly."
 
 
 KeyMapper()
